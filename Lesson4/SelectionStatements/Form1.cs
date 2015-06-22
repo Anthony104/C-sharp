587d8480ecfaa6421ee0bad42cac720c9ec50589
unknown <blue12@RM02-12.fasfg.local>
2015-06-22 15:06:48 +0100

    Statements

---
+++ Lesson4/SelectionStatements/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SelectionStatements
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Anthony")
+            {
+                MessageBox.Show("you picked Anthony");
+            }
+            else if (comboBox1.Text == "Tony")
+            {
+                MessageBox.Show("you picked Tony");
+            }
+
+            else if (comboBox1.Text == "Anto")
+            {
+                MessageBox.Show("you picked Anto");
+            }
+
+            else
+            {
+                MessageBox.Show("ERROR!!!!");
+            }
+        }
+    }
+}
