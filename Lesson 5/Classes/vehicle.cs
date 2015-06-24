612b15df1eb5feec51e202f1b6d0b72ec684bcca
unknown <blue12@RM02-12.fasfg.local>
2015-06-24 15:19:00 +0100

    Classes

---
+++ Lesson 5/Classes/vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class vehicle
+    {
+        private string _make;
+        private string _model;
+        private int _elapsedMilage; 
+
+        public string Make
+        {
+            get { return _make; }
+            set { _make = value; }
+        }
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        public int ElapsedMilage
+        {
+            get { return _elapsedMilage; }
+            set { _elapsedMilage = value; }
+        }
+    }
+}
